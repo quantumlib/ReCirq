f4e5b8fb6949256dcea64220a943708ab85e88c5
Matthew Harrigan <mpharrigan@google.com>
2020-03-23 18:36:58 -0700

    QAOA - Circuit Structure

---
+++ recirq/qaoa/circuit_structure.py
@@ -0,0 +1,180 @@
+from dataclasses import dataclass
+from typing import Callable
+
+import numpy as np
+
+import cirq
+import cirq.google as cg
+try:
+    from cirq.interop.quirk import QuirkQubitPermutationGate
+except ImportError:
+    from cirq.contrib.quirk import QuirkQubitPermutationGate
+
+
+@dataclass(frozen=True)
+class HomogeneousCircuitStats:
+    num_phx: int
+    num_z: int
+    num_syc: int
+    has_permutation: bool = False
+    has_measurement: bool = False
+
+
+def _homogeneous_moment(
+        moment: 'cirq.Moment',
+        op_condition: Callable[[cirq.Operation], bool]) -> bool:
+    cases = {op_condition(op) for op in moment}
+    if len(cases) != 1:
+        # Non-homogeneous
+        return False
+    return list(cases)[0]
+
+
+def _homogeneous_gate_type(moment, gate_type):
+    return _homogeneous_moment(
+        moment,
+        lambda op: op.gate is not None and isinstance(op.gate, gate_type))
+
+
+MOMENT_GATE_CLASSES = [
+    cirq.PhasedXPowGate,
+    cirq.ZPowGate,
+    cg.SycamoreGate,
+    cirq.MeasurementGate,
+    QuirkQubitPermutationGate,
+]
+
+
+def get_moment_class(moment: cirq.Moment):
+    """For a given moment, return the Gate class which every operation
+    is an instantiation or `None` for non-homogeneous or non-hardware moments.
+    """
+    mom_class_vec = [_homogeneous_gate_type(moment, gc) for gc in MOMENT_GATE_CLASSES]
+    if np.sum(mom_class_vec) != 1:
+        # Non-homogeneous or not in gcs
+        return None
+
+    return MOMENT_GATE_CLASSES[np.argmax(mom_class_vec)]
+
+
+def get_moment_classes(circuit: cirq.Circuit):
+    """Return the 'moment class' for each moment in the circuit.
+
+    A moment class is the Gate class of which every operation
+    is an instantiation or `None` for non-homogeneous or non-hardware moments.
+    """
+    return [get_moment_class(moment) for moment in circuit.moments]
+
+
+def _find_circuit_structure_violations(mom_classes):
+    violations = np.asarray([mom_class is None for mom_class in mom_classes])
+    violation_indices = np.where(violations)[0]
+    return violation_indices
+
+
+def find_circuit_structure_violations(circuit: cirq.Circuit):
+    """Return indices where the circuit contains non-homogenous
+    non-hardware moments.
+    """
+    mom_classes = get_moment_classes(circuit)
+    return _find_circuit_structure_violations(mom_classes)
+
+class BadlyStructuredCircuitError(ValueError):
+    pass
+
+def validate_well_structured(circuit: cirq.Circuit,
+                             allow_terminal_permutations=False):
+    """Raises a ValueError if the circuit is not structured, otherwise returns
+    a list of moment classes (see `get_moment_classes`) and a
+    `HomogeneousCircuitStats` set of statistics.
+
+    A "structured circuit" means that each moment contains either all-PhX,
+    all-Z, all-SYC, or all-SQRT_ISWAP (i.e. moments have homogeneous gate types).
+    The gate type common in a given moment can be called the "moment class".
+    In structured circuits, moments are arranged so that the layers are always
+    PhX, Z, (SYC/SQRT_ISWAP) for many layers, optionally ending with a
+    measurement layer.
+
+    If `allow_terminal_permutations` is set to `True`, a
+    `QuirkQubitPermutationGate` is permitted preceding a measurement.
+    Use `circuits2.measure_with_final_permutation` to append a measurement
+    gate and track the permutation for implementation via post-processing.
+    """
+    mom_classes = get_moment_classes(circuit)
+    violation_indices = _find_circuit_structure_violations(mom_classes)
+    if len(violation_indices) > 0:
+        raise BadlyStructuredCircuitError(
+            "Badly structured circuit. "
+            "Inhomogeneous or non-device moments at indices {}"
+                .format(violation_indices))
+
+    # Check that there's only one layer of PhX or Z between entangling layers
+    # Check that permutations and measurements must come at the end (in that order)
+    # Also count totals while we're at it.
+    num_phx = 0
+    num_z = 0
+    hit_permutation = False
+    hit_measurement = False
+
+    tot_n_phx = 0
+    tot_n_z = 0
+    tot_n_syc = 0
+    for mom_class in mom_classes:
+        if mom_class in [cirq.PhasedXPowGate, cirq.ZPowGate, cg.SycamoreGate]:
+            if hit_permutation:
+                raise BadlyStructuredCircuitError(
+                    "Permutations must be terminal")
+            if hit_measurement:
+                raise BadlyStructuredCircuitError(
+                    "Measurements must be terminal")
+
+        if mom_class == cirq.PhasedXPowGate:
+            num_phx += 1
+            tot_n_phx += 1
+        elif mom_class == cirq.ZPowGate:
+            num_z += 1
+            tot_n_z += 1
+        elif mom_class == cg.SycamoreGate:
+            tot_n_syc += 1
+            if num_phx > 1:
+                raise BadlyStructuredCircuitError("Too many PhX in this slice")
+            if num_z > 1:
+                raise BadlyStructuredCircuitError("Too many Z in this slice")
+            if num_phx < 1:
+                print("Warning: no PhX in this slice")
+
+            num_phx = 0
+            num_z = 0
+        elif mom_class == cirq.MeasurementGate:
+            if hit_measurement:
+                raise BadlyStructuredCircuitError("Too many measurements")
+            if hit_permutation:
+                pass  # fine
+            hit_measurement = True
+        elif mom_class == QuirkQubitPermutationGate:
+            if not allow_terminal_permutations:
+                raise BadlyStructuredCircuitError(
+                    "Circuit contains permutation gates")
+
+            if hit_measurement:
+                raise BadlyStructuredCircuitError("Measurements must be terminal")
+            if hit_permutation:
+                raise BadlyStructuredCircuitError("Too many permutations")
+            hit_permutation = True
+        else:
+            raise BadlyStructuredCircuitError("Unknown moment class")
+
+    return mom_classes, HomogeneousCircuitStats(tot_n_phx, tot_n_z, tot_n_syc,
+                                                hit_permutation,
+                                                hit_measurement)
+
+
+def idle_qubits_by_moment(circuit: cirq.Circuit):
+    qubits = circuit.all_qubits()
+    for i, moment in enumerate(circuit.moments):
+        idle_qubits = qubits - moment.qubits
+        yield i, idle_qubits
+
+
+def count_circuit_holes(circuit: cirq.Circuit):
+    return sum(len(iq) for _, iq in idle_qubits_by_moment(circuit))
---
+++ recirq/qaoa/circuit_structure_test.py
@@ -0,0 +1,165 @@
+import networkx as nx
+import numpy as np
+import pytest
+
+import cirq
+import cirq.google as cg
+
+try:
+    from cirq.interop.quirk import QuirkQubitPermutationGate
+except ImportError:
+    from cirq.contrib.quirk import QuirkQubitPermutationGate
+
+from recirq.qaoa.circuit_structure import get_moment_class, get_moment_classes, \
+    find_circuit_structure_violations, BadlyStructuredCircuitError, validate_well_structured
+from recirq.qaoa.gates_and_compilation import ProblemUnitary, DriverUnitary, \
+    compile_problem_unitary_to_swap_network, compile_swap_network_to_zzswap, \
+    compile_driver_unitary_to_rx, compile_to_syc
+from recirq.qaoa.problems import random_plus_minus_1_weights
+
+
+def test_get_moment_class():
+    q0, q1 = cirq.LineQubit.range(2)
+
+    # Non-hardware
+    assert get_moment_class(cirq.Moment([cirq.CNOT(q0, q1)])) is None
+    # Non-homogenous
+    assert get_moment_class(cirq.Moment([cirq.X(q0), cirq.Z(q1)])) is None
+
+    # Both phasedxpow
+    assert get_moment_class(cirq.Moment([
+        cirq.PhasedXPowGate(phase_exponent=0).on(q0),
+        cirq.PhasedXPowGate(phase_exponent=0.5).on(q1)])) \
+           == cirq.PhasedXPowGate
+
+    # Both Z
+    assert get_moment_class(cirq.Moment([
+        cirq.ZPowGate(exponent=0.123).on(q0),
+        cirq.ZPowGate(exponent=0.5).on(q1)])) \
+           == cirq.ZPowGate
+
+    # SYC
+    assert get_moment_class(cirq.Moment([cg.SYC(q0, q1)])) == cg.SycamoreGate
+
+    # Measurement
+    assert get_moment_class(cirq.Moment([cirq.measure(q0, q1)])) == cirq.MeasurementGate
+
+    # Permutation
+    assert get_moment_class(cirq.Moment([
+        QuirkQubitPermutationGate('', '', [1, 0]).on(q0, q1)])) \
+           == QuirkQubitPermutationGate
+
+
+def test_get_moment_classes():
+    n = 5
+    problem = nx.complete_graph(n=n)
+    problem = random_plus_minus_1_weights(problem)
+    qubits = cirq.LineQubit.range(n)
+    p = 1
+    c1 = cirq.Circuit(
+        cirq.H.on_each(qubits),
+        [
+            [
+                ProblemUnitary(problem, gamma=np.random.random()).on(*qubits),
+                DriverUnitary(5, beta=np.random.random()).on(*qubits)
+            ]
+            for _ in range(p)
+        ]
+    )
+    c2 = compile_problem_unitary_to_swap_network(c1)
+    c3 = compile_swap_network_to_zzswap(c2)
+    c4 = compile_driver_unitary_to_rx(c3)
+    c5 = compile_to_syc(c4)
+    mom_classes = get_moment_classes(c5)
+    should_be = [cirq.PhasedXPowGate, cirq.ZPowGate, cg.SycamoreGate] * (n * p * 3)
+    should_be += [cirq.PhasedXPowGate, cirq.ZPowGate, QuirkQubitPermutationGate]
+    assert mom_classes == should_be
+
+
+def test_find_circuit_structure_violations():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit([
+        cirq.Moment([cirq.PhasedXPowGate(phase_exponent=0).on(q0)]),
+        cirq.Moment([
+            cirq.PhasedXPowGate(phase_exponent=0.5).on(q0),
+            cirq.ZPowGate(exponent=1).on(q1)]),
+        cirq.Moment([cg.SYC(q0, q1)]),
+        cirq.measure(q0, q1, key='z'),
+        cirq.CNOT(q0, q1)
+    ])
+    violations = find_circuit_structure_violations(circuit)
+    np.testing.assert_array_equal(violations, [1, 4])
+    assert violations.tolist() == [1, 4]
+
+
+def test_validate_well_structured_inhomo():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit([
+        cirq.Moment([cirq.PhasedXPowGate(phase_exponent=0).on(q0)]),
+        cirq.Moment([
+            cirq.PhasedXPowGate(phase_exponent=0.5).on(q0),
+            cirq.ZPowGate(exponent=1).on(q1)]),
+        cirq.Moment([cg.SYC(q0, q1)]),
+        cirq.measure(q0, q1, key='z'),
+    ])
+
+    with pytest.raises(BadlyStructuredCircuitError) as e:
+        validate_well_structured(circuit)
+    assert e.match('Inhomogeneous')
+
+
+def test_validate_well_structured_bad_gate():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit([
+        cirq.Moment([cirq.PhasedXPowGate(phase_exponent=0).on(q0)]),
+        cirq.Moment([
+            cirq.XPowGate(exponent=0.5).on(q0), ]),
+        cirq.Moment([cg.SYC(q0, q1)]),
+        cirq.measure(q0, q1, key='z'),
+    ])
+
+    with pytest.raises(BadlyStructuredCircuitError) as e:
+        validate_well_structured(circuit)
+    assert e.match('non-device')
+
+
+def test_validate_well_structured_too_many():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit([
+        cirq.Moment([cirq.PhasedXPowGate(phase_exponent=0).on(q0)]),
+        cirq.Moment([
+            cirq.PhasedXPowGate(phase_exponent=0.5).on(q0), ]),
+        cirq.Moment([cg.SYC(q0, q1)]),
+        cirq.measure(q0, q1, key='z'),
+    ])
+
+    with pytest.raises(BadlyStructuredCircuitError) as e:
+        validate_well_structured(circuit)
+    assert e.match('Too many PhX')
+
+
+def test_validate_well_structured_non_term_meas():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit([
+        cirq.Moment([cirq.PhasedXPowGate(phase_exponent=0).on(q0)]),
+        cirq.Moment([
+            cirq.PhasedXPowGate(phase_exponent=0.5).on(q0), ]),
+        cirq.measure(q0, q1, key='z'),
+        cirq.Moment([cg.SYC(q0, q1)]),
+    ])
+
+    with pytest.raises(BadlyStructuredCircuitError) as e:
+        validate_well_structured(circuit)
+    assert e.match('Measurements must be terminal')
+
+
+def test_validate_well_structured():
+    q0, q1 = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit([
+        cirq.Moment([cirq.PhasedXPowGate(phase_exponent=0).on(q0)]),
+        cirq.Moment([
+            cirq.ZPowGate(exponent=0.5).on(q0), ]),
+        cirq.Moment([cg.SYC(q0, q1)]),
+        cirq.measure(q0, q1, key='z'),
+    ])
+    validate_well_structured(circuit)
