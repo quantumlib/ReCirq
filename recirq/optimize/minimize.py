a58f56c8a19163442db28c8d83d53f75263f21af
JiahaoYao <jiahaoyao.math@gmail.com>
2020-07-17 23:01:03 -0700

    add the mpg signature

--- recirq/optimize/minimize.py
+++ recirq/optimize/minimize.py
@@ -3,8 +3,10 @@ from typing import Callable, Optional, Tuple
 import numpy as np
 import scipy.optimize
 from recirq.optimize.mgd import model_gradient_descent
+from recirq.optimize.mpg import model_policy_gradient
 
-OPTIMIZERS = {'mgd': model_gradient_descent}
+
+OPTIMIZERS = {'mgd': model_gradient_descent, 'mpg': model_policy_gradient}
 
 
 def minimize(fun: Callable[..., float],
@@ -25,7 +27,7 @@ def minimize(fun: Callable[..., float],
         args: Extra arguments passed to the objective function.
         method: The name of the optimization method to use. Can be any method
             supported by `scipy.optimize.minimize`, or else a method implemented
-            in ReCirq. Currently the only method implemented in ReCirq is 'MGD'.
+            in ReCirq. Currently the methods implemented in ReCirq are 'MGD' and 'mpg'.
     """
     if method.lower() in OPTIMIZERS:
         optimizer = OPTIMIZERS[method.lower()]
