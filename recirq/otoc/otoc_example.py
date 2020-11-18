8697408a449490423d1d1c53a2523c769a215093
mixiao <mixiao@google.com>
2020-11-18 14:51:34 -0800

    Initial commit

--- recirq/otoc/otoc_example.py
+++ recirq/otoc/otoc_example.py
@@ -1,76 +1,109 @@
+# Copyright 2020 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example for measuring OTOC of several random circuits."""
+
 import cirq
 import numpy as np
 from matplotlib import pyplot as plt
 
 from recirq.otoc.otoc_circuits import build_otoc_circuits
 
-project_id = 'xiaomidec2018'
-engine = cirq.google.Engine(project_id=project_id,
-                            proto_version=cirq.google.ProtoVersion.V2)
-processor_name = 'rainbow'
 
-qubit_locs = [(3, 3), (2, 3), (2, 4), (2, 5), (1, 5), (0, 5), (0, 6), (1, 6)]
-qubits = [cirq.GridQubit(*idx) for idx in qubit_locs]
-ancilla_loc = (3, 2)
-ancilla = cirq.GridQubit(*ancilla_loc)
-num_qubits = len(qubits)
+def main():
+    # Specify project ID and processor name.
+    project_id = ''
+    engine = cirq.google.Engine(project_id=project_id,
+                                proto_version=cirq.google.ProtoVersion.V2)
+    processor_name = 'rainbow'
 
-int_sets = [{(qubits[i], qubits[i + 1]) for i in range(0, num_qubits - 1, 2)},
-            {(qubits[i], qubits[i + 1]) for i in range(1, num_qubits - 1, 2)}]
+    # Specify qubits to measure. The qubits must form a line. The ancilla qubit
+    # must be connected to the first qubit.
+    qubit_locs = [(3, 3), (2, 3), (2, 4), (2, 5), (1, 5), (0, 5), (0, 6),
+                  (1, 6)]
+    qubits = [cirq.GridQubit(*idx) for idx in qubit_locs]
+    ancilla_loc = (3, 2)
+    ancilla = cirq.GridQubit(*ancilla_loc)
+    num_qubits = len(qubits)
 
-forward_ops = {(qubit_locs[i], qubit_locs[i + 1]): cirq.Circuit([cirq.CZ(
-    qubits[i], qubits[i + 1]) ** 1]) for i in range(num_qubits - 1)}
+    # Specify how the qubit interact. For the 1D chain in this example, only two
+    # layers of two-qubit gates (CZ is used here) is needed to enact all
+    # interactions.
+    int_sets = [
+        {(qubits[i], qubits[i + 1]) for i in range(0, num_qubits - 1, 2)},
+        {(qubits[i], qubits[i + 1]) for i in range(1, num_qubits - 1, 2)}]
 
-reverse_ops = {(qubit_locs[i], qubit_locs[i + 1]): cirq.Circuit([cirq.CZ(
-    qubits[i], qubits[i + 1]) ** -1]) for i in range(num_qubits - 1)}
+    forward_ops = {(qubit_locs[i], qubit_locs[i + 1]): cirq.Circuit([cirq.CZ(
+        qubits[i], qubits[i + 1])]) for i in range(num_qubits - 1)}
 
-circuit_list = []
-cycles = range(12)
-num_trials = 2
-for i in range(num_trials):
-    np.random.seed(i)
-    rand_nums = np.random.choice(8, (num_qubits, max(cycles)))
-    circuits_i = []
-    for cycle in cycles:
-        circuits_ic = []
-        for k, q_b in enumerate(qubits[1:]):
-            circs = build_otoc_circuits(
-                qubits, ancilla, cycle, int_sets, forward_ops=forward_ops,
-                reverse_ops=reverse_ops, butterfly_qubits=q_b,
-                cycles_per_echo=2, sq_gates=rand_nums, use_physical_cz=True)
-            if k == 0:
-                circuits_ic.extend(circs[0:8])
-            else:
-                circuits_ic.extend(circs[4:8])
-        circuits_i.append(circuits_ic)
-    circuit_list.append(circuits_i)
+    reverse_ops = {(qubit_locs[i], qubit_locs[i + 1]): cirq.Circuit([cirq.CZ(
+        qubits[i], qubits[i + 1])]) for i in range(num_qubits - 1)}
 
-results = []
-for i, circuits_i in enumerate(circuit_list):
-    results_i = np.zeros((num_qubits, len(cycles)))
-    for c, circuits_ic in enumerate(circuits_i):
-        print('Measuring circuit instance {}, cycle {}...'.format(i, c))
-        stats = int(2000 + 15000 * (c / max(cycles)) ** 3)
-        sweep_params = [{} for _ in range(len(circuits_ic))]
-        job = engine.run_batch(
-            programs=circuits_ic,
-            params_list=sweep_params,
-            repetitions=stats,
-            processor_ids=[processor_name],
-            gate_set=cirq.google.FSIM_GATESET)
-        for d in range(num_qubits):
-            p = np.mean(job.results()[4 * d].measurements['z'])
-            p -= np.mean(job.results()[4 * d + 1].measurements['z'])
-            p -= np.mean(job.results()[4 * d + 2].measurements['z'])
-            p += np.mean(job.results()[4 * d + 3].measurements['z'])
-            results_i[d, c] = 0.5 * p
-        results.append(results_i)
+    # Build two random circuit instances (each having 12 cycles).
+    circuit_list = []
+    cycles = range(12)
+    num_trials = 2
+    for i in range(num_trials):
+        np.random.seed(i)
+        rand_nums = np.random.choice(8, (num_qubits, max(cycles)))
+        circuits_i = []
+        for cycle in cycles:
+            circuits_ic = []
+            for k, q_b in enumerate(qubits[1:]):
+                circs = build_otoc_circuits(
+                    qubits, ancilla, cycle, int_sets, forward_ops=forward_ops,
+                    reverse_ops=reverse_ops, butterfly_qubits=q_b,
+                    cycles_per_echo=2, sq_gates=rand_nums, use_physical_cz=True)
+                if k == 0:
+                    circuits_ic.extend(circs[0:8])
+                else:
+                    circuits_ic.extend(circs[4:8])
+            circuits_i.append(circuits_ic)
+        circuit_list.append(circuits_i)
 
-results_ave = np.mean(results, axis=0)
-fig = plt.figure()
-plt.plot(results_ave[0, :], 'ko-', label='Normalization')
-for i in range(1, num_qubits):
-    plt.plot(results_ave[i, :], 'o-', label='Qubit {}'.format(i + 1))
-plt.xlabel('Cycles')
-plt.ylabel(r'$\langle \sigma_y \rangle$')
-plt.legend()
+    # Measure the OTOCs of the two random circuits.
+    results = []
+    for i, circuits_i in enumerate(circuit_list):
+        results_i = np.zeros((num_qubits, len(cycles)))
+        for c, circuits_ic in enumerate(circuits_i):
+            print('Measuring circuit instance {}, cycle {}...'.format(i, c))
+            stats = int(2000 + 10000 * (c / max(cycles)) ** 3)
+            sweep_params = [{} for _ in range(len(circuits_ic))]
+            job = engine.run_batch(
+                programs=circuits_ic,
+                params_list=sweep_params,
+                repetitions=stats,
+                processor_ids=[processor_name],
+                gate_set=cirq.google.FSIM_GATESET)
+            for d in range(num_qubits):
+                p = np.mean(job.results()[4 * d].measurements['z'])
+                p -= np.mean(job.results()[4 * d + 1].measurements['z'])
+                p -= np.mean(job.results()[4 * d + 2].measurements['z'])
+                p += np.mean(job.results()[4 * d + 3].measurements['z'])
+                results_i[d, c] = 0.5 * p
+            results.append(results_i)
+
+    # Average the data for the two random circuits and plot out results.
+    results_ave = np.mean(results, axis=0)
+    fig = plt.figure()
+    plt.plot(results_ave[0, :], 'ko-', label='Normalization')
+    for i in range(1, num_qubits):
+        plt.plot(results_ave[i, :], 'o-', label='Qubit {}'.format(i + 1))
+    plt.xlabel('Cycles')
+    plt.ylabel(r'$\langle \sigma_y \rangle$')
+    plt.legend()
+
+
+if __name__ == '__main__':
+    main()
--- recirq/otoc/parallel_xeb_example.py
+++ recirq/otoc/parallel_xeb_example.py
@@ -1,73 +1,100 @@
+# Copyright 2020 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Example for performing parallel-XEB and calibrating unitary shifts."""
+
 import cirq
 import numpy as np
 
 from recirq.otoc.parallel_xeb import build_xeb_circuits, parallel_xeb_fidelities
-from recirq.otoc.utils import cz_to_sqrt_iswap, save_data
+from recirq.otoc.utils import save_data
 
-dir_str = '/usr/local/google/home/mixiao/PycharmProjects/ReCirq/recirq/otoc' \
-          '/data'
-project_id = 'xiaomidec2018'
-engine = cirq.google.Engine(project_id=project_id,
-                            proto_version=cirq.google.ProtoVersion.V2)
-processor_name = 'mcgee'
 
-qubit_locs = [(3, 3), (2, 3), (2, 4), (2, 5), (1, 5), (0, 5), (0, 6), (1, 6)]
-qubits = [cirq.GridQubit(*idx) for idx in qubit_locs]
-ancilla_loc = (4, 3)
-ancilla = cirq.GridQubit(*ancilla_loc)
-num_qubits = len(qubits)
+def main():
+    # Specify a working directory, project ID and processor name.
+    dir_str = ''
+    project_id = ''
+    engine = cirq.google.Engine(project_id=project_id,
+                                proto_version=cirq.google.ProtoVersion.V2)
+    processor_name = 'rainbow'
 
-int_layers = [
-    {(qubit_locs[i], qubit_locs[i + 1]) for i in range(0, num_qubits - 1, 2)},
-    {(qubit_locs[i], qubit_locs[i + 1]) for i in range(1, num_qubits - 1, 2)}]
+    # Specify qubits to measure. Here we choose the qubits to be on a line.
+    qubit_locs = [(3, 3), (2, 3), (2, 4), (2, 5), (1, 5), (0, 5), (0, 6),
+                  (1, 6)]
+    qubits = [cirq.GridQubit(*idx) for idx in qubit_locs]
+    num_qubits = len(qubits)
 
-xeb_configs = [[cirq.Moment([cirq.ISWAP(qubits[i], qubits[i + 1]) ** 0.5
-                             for i in range(0, num_qubits - 1, 2)])],
-               [cirq.Moment([cirq.ISWAP(qubits[i], qubits[i + 1]) ** 0.5
-                             for i in range(1, num_qubits - 1, 2)])],
-               [cirq.Moment([cirq.ISWAP(qubits[i], qubits[i + 1]) ** -0.5
-                             for i in range(0, num_qubits - 1, 2)])],
-               [cirq.Moment([cirq.ISWAP(qubits[i], qubits[i + 1]) ** -0.5
-                             for i in range(1, num_qubits - 1, 2)])],
-               [cz_to_sqrt_iswap(ancilla, qubits[0])]]
+    # Specify the gate layers to calibrate. For qubits on a line, all two-qubit
+    # gates can be calibrated in two layers.
+    int_layers = [
+        {(qubit_locs[i], qubit_locs[i + 1]) for i in
+         range(0, num_qubits - 1, 2)},
+        {(qubit_locs[i], qubit_locs[i + 1]) for i in
+         range(1, num_qubits - 1, 2)}]
 
-num_trials = 50
-num_cycle_range = range(3, 75, 10)
-num_cycles = len(num_cycle_range)
+    xeb_configs = [[cirq.Moment([cirq.ISWAP(qubits[i], qubits[i + 1]) ** 0.5
+                                 for i in range(0, num_qubits - 1, 2)])],
+                   [cirq.Moment([cirq.ISWAP(qubits[i], qubits[i + 1]) ** 0.5
+                                 for i in range(1, num_qubits - 1, 2)])]]
 
-all_bits_sqrtiswap = []
-all_phases_sqrtiswap = []
-for xeb_config in xeb_configs[0:2]:
-    bits_sqrtiswap = []
-    phases_sqrtiswap = []
-    for i in range(num_trials):
-        print(i)
-        circuits, rand_indices = build_xeb_circuits(
-            qubits, num_cycle_range, xeb_config, random_seed=i)
-        phases_sqrtiswap.append(rand_indices)
-        for c in circuits:
-            c.append(cirq.measure(*qubits, key='z'))
-        sweep_params = [{} for _ in range(len(circuits))]
-        job = engine.run_batch(
-            programs=circuits,
-            params_list=sweep_params,
-            repetitions=5000,
-            processor_ids=[processor_name],
-            gate_set=cirq.google.SQRT_ISWAP_GATESET)
-        bits_sqrtiswap.append([job.results()[j].measurements['z'] for j in
-                               range(num_cycles)])
-    all_bits_sqrtiswap.append(bits_sqrtiswap)
-    all_phases_sqrtiswap.append(phases_sqrtiswap)
+    # Specify the number of random circuits in parallel XEB the cycles at which
+    # fidelities are to be measured.
+    num_trials = 20
+    num_cycle_range = range(3, 75, 10)
+    num_cycles = len(num_cycle_range)
 
-fsim_angles_0 = {'theta': -0.25 * np.pi, 'delta_plus': 0,
-                 'delta_minus_off_diag': 0, 'delta_minus_diag': 0,
-                 'phi': 0.0}
-(fitted_gates_0, corrected_gates_0, fsim_fitted_0, err_0_opt,
- err_0_unopt) = parallel_xeb_fidelities(
-    qubit_locs, num_cycle_range, all_bits_sqrtiswap,
-    all_phases_sqrtiswap, fsim_angles_0,
-    interaction_sequence=int_layers, gate_to_fit='sqrt-iswap',
-    plot_individual_traces=True, num_restarts=5,
-    plot_histograms=True, save_directory=None)
+    # Generate random XEB circuits, measure the resulting bit-strings, and save
+    # the data as well as random circuit information.
+    all_bits_sqrtiswap = []
+    all_phases_sqrtiswap = []
+    for xeb_config in xeb_configs:
+        bits_sqrtiswap = []
+        phases_sqrtiswap = []
+        for i in range(num_trials):
+            print(i)
+            circuits, rand_indices = build_xeb_circuits(
+                qubits, num_cycle_range, xeb_config, random_seed=i)
+            phases_sqrtiswap.append(rand_indices)
+            for c in circuits:
+                c.append(cirq.measure(*qubits, key='z'))
+            sweep_params = [{} for _ in range(len(circuits))]
+            job = engine.run_batch(
+                programs=circuits,
+                params_list=sweep_params,
+                repetitions=5000,
+                processor_ids=[processor_name],
+                gate_set=cirq.google.SQRT_ISWAP_GATESET)
+            bits_sqrtiswap.append([job.results()[j].measurements['z'] for j in
+                                   range(num_cycles)])
+        all_bits_sqrtiswap.append(bits_sqrtiswap)
+        all_phases_sqrtiswap.append(phases_sqrtiswap)
 
-save_data(corrected_gates_0, dir_str + '/gate_corrections')
+    # Perform fits on each qubit pair to miminize the cycle errors. The fitting
+    # results are saved to the working directory.
+    fsim_angles_0 = {'theta': -0.25 * np.pi, 'delta_plus': 0,
+                     'delta_minus_off_diag': 0, 'delta_minus_diag': 0,
+                     'phi': 0.0}
+    (fitted_gates_0, corrected_gates_0, fsim_fitted_0, err_0_opt,
+     err_0_unopt) = parallel_xeb_fidelities(
+        qubit_locs, num_cycle_range, all_bits_sqrtiswap,
+        all_phases_sqrtiswap, fsim_angles_0,
+        interaction_sequence=int_layers, gate_to_fit='sqrt-iswap',
+        plot_individual_traces=True, num_restarts=5,
+        plot_histograms=True, save_directory=None)
+
+    save_data(corrected_gates_0, dir_str + '/gate_corrections')
+
+
+if __name__ == '__main__':
+    main()
