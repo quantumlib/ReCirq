c873c64b416a9325c7fdebf210d8412dd1781ab2
Matthew Harrigan <mpharrigan@google.com>
2020-11-30 11:02:08 -0800

    Install dependencies and format hfvqe/quickstart (#102)
    * Install dependencies and format hfvqe/quickstart
    
    Partially address #101. Re-work `molecular_exmaple.py` to
    support putting these data files elsewhere in the future.
    
    * Spruce up headings

--- recirq/hfvqe/molecular_example.py
+++ recirq/hfvqe/molecular_example.py
@@ -23,16 +23,17 @@ from recirq.hfvqe.gradient_hf import rhf_minimization
 from recirq.hfvqe.objective import (RestrictedHartreeFockObjective,
                                     generate_hamiltonian)
 
+_MOLECULAR_DATA_DIRECTORY = os.path.dirname(os.path.abspath(__file__)) + '/molecular_data'
 
-def make_h6_1_3() -> Tuple[RestrictedHartreeFockObjective, of.MolecularData, np.
-                           ndarray, np.ndarray, np.ndarray]:
-    # load the molecule from moelcular data
-    import recirq.hfvqe as hfvqe
-    h6_1_3_path = os.path.join(
-        hfvqe.__path__[0],
-        'molecular_data/hydrogen_chains/h_6_sto-3g/bond_distance_1.3')
 
-    molfile = os.path.join(h6_1_3_path, 'H6_sto-3g_singlet_linear_r-1.3.hdf5')
+def make_h6_1_3(molecular_data_directory=None) \
+        -> Tuple[RestrictedHartreeFockObjective, of.MolecularData,
+                 np.ndarray, np.ndarray, np.ndarray]:
+    if molecular_data_directory is None:
+        molecular_data_directory = _MOLECULAR_DATA_DIRECTORY
+
+    h6_1_3_path = f'{molecular_data_directory}/hydrogen_chains/h_6_sto-3g/bond_distance_1.3'
+    molfile = f'{h6_1_3_path}/H6_sto-3g_singlet_linear_r-1.3.hdf5'
     molecule = of.MolecularData(filename=molfile)
     molecule.load()
 
@@ -51,4 +52,32 @@ def make_h6_1_3() -> Tuple[RestrictedHartreeFockObjective, of.MolecularData, np.
 
     scipy_result = rhf_minimization(rhf_objective)
 
+    return rhf_objective, molecule, scipy_result.x, obi, tbi
+
+
+def make_h3_2_5(molecular_data_directory=None) \
+        -> Tuple[RestrictedHartreeFockObjective, of.MolecularData,
+                 np.ndarray, np.ndarray, np.ndarray]:
+    if molecular_data_directory is None:
+        molecular_data_directory = _MOLECULAR_DATA_DIRECTORY
+
+    h3_2_5_path = f'{molecular_data_directory}/hydrogen_chains/h_3_p_sto-3g/bond_distance_2.5'
+    molfile = f'{h3_2_5_path}/H3_plus_sto-3g_singlet_linear_r-2.5.hdf5'
+    molecule = of.MolecularData(filename=molfile)
+    molecule.load()
+
+    S = np.load(os.path.join(h3_2_5_path, 'overlap.npy'))
+    Hcore = np.load(os.path.join(h3_2_5_path, 'h_core.npy'))
+    TEI = np.load(os.path.join(h3_2_5_path, 'tei.npy'))
+
+    _, X = sp.linalg.eigh(Hcore, S)
+    obi = of.general_basis_change(Hcore, X, (1, 0))
+    tbi = np.einsum('psqr', of.general_basis_change(TEI, X, (1, 0, 1, 0)))
+    molecular_hamiltonian = generate_hamiltonian(obi, tbi,
+                                                 molecule.nuclear_repulsion)
+
+    rhf_objective = RestrictedHartreeFockObjective(molecular_hamiltonian,
+                                                   molecule.n_electrons)
+
+    scipy_result = rhf_minimization(rhf_objective)
     return rhf_objective, molecule, scipy_result.x, obi, tbi
