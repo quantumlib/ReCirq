3ae97ff74a0d0f92fcf25b7096d8eaaa22c1990c
Doug Strain <dstrain@google.com>
2021-02-01 16:05:23 -0800

    Fix batch_move to use correct gateset
    - QuantumProcessor uses a string to identify gateset instead
    of a cirq object, so change the function to use this string.

--- recirq/quantum_chess/experiments/batch_moves.py
+++ recirq/quantum_chess/experiments/batch_moves.py
@@ -32,7 +32,7 @@ import recirq.quantum_chess.quantum_board as qb
 def create_board(processor_name: str, *, noise_mitigation: float):
     return qb.CirqBoard(init_basis_state=0,
                         sampler=utils.get_sampler_by_name(
-                            processor_name, gateset=cg.SQRT_ISWAP_GATESET),
+                            processor_name, gateset='sqrt-iswap'),
                         device=utils.get_device_obj_by_name(processor_name),
                         error_mitigation=enums.ErrorMitigation.Correct,
                         noise_mitigation=noise_mitigation)
