4819526c8ef0dc5d54e98053fdf5611674cd2d79
madcpf <madcpf@gmail.com>
2021-08-30 22:00:09 -0700

    trigger test

--- recirq/quantum_chess/pauli_decomposition.py
+++ recirq/quantum_chess/pauli_decomposition.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Utility functions used for decomposing the given measurement matrix into a PauliSum.
+Utility functions used for decomposing the given measurement matrix into a PauliSumm.
 """
 import cirq
 import itertools
