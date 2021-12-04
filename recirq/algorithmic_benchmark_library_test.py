3308c5e2fdff9b87e6c49d2b27863d630968525b
Matthew Harrigan <mpharrigan@google.com>
2021-12-03 16:38:58 -0800

    assert messages
    Change-Id: I7a78669ee18d47a4e909783663c2da210d1db699

--- recirq/algorithmic_benchmark_library_test.py
+++ recirq/algorithmic_benchmark_library_test.py
@@ -12,9 +12,9 @@ RECIRQ_DIR = os.path.abspath(os.path.dirname(__file__) + '/../')
 @pytest.mark.parametrize('algo', BENCHMARKS)
 def test_domain(algo):
     # By convention, the domain should be a recirq module.
-    assert algo.domain.startswith('recirq.')
+    assert algo.domain.startswith('recirq.'), 'domain should be a recirq module.'
     mod = import_module(algo.domain)
-    assert isinstance(mod, ModuleType)
+    assert isinstance(mod, ModuleType), 'domain should be a recirq module.'
 
 
 def test_benchmark_name_unique_in_domain():
@@ -26,24 +26,30 @@ def test_benchmark_name_unique_in_domain():
 @pytest.mark.parametrize('algo', BENCHMARKS)
 def test_executable_family_is_formulaic(algo):
     # Check consistency in the AlgorithmicBenchmark dataclass:
-    assert algo.executable_family == algo.spec_class.executable_family
+    assert algo.executable_family == algo.spec_class.executable_family, \
+        "benchmark's executable_family should match that of the spec_class"
 
     # By convention, we set this to be the module name. By further convention,
     # {algo.domain}.{algo.name} should be the module name.
-    assert algo.executable_family == f'{algo.domain}.{algo.name}'
+    assert algo.executable_family == f'{algo.domain}.{algo.name}', \
+        "The executable family should be set to the benchmarks's domain.name"
 
     # Check the convention that it should give a module
     mod = import_module(algo.executable_family)
-    assert isinstance(mod, ModuleType)
+    assert isinstance(mod, ModuleType), \
+        "The executable family should specify an importable module."
 
 
 @pytest.mark.parametrize('algo', BENCHMARKS)
 def test_classes_and_funcs(algo):
     # The various class objects should exist in the module
     mod = import_module(algo.executable_family)
-    assert algo.spec_class == getattr(mod, algo.spec_class.__name__)
-    assert algo.data_class == getattr(mod, algo.data_class.__name__)
-    assert algo.executable_generator_func == getattr(mod, algo.executable_generator_func.__name__)
+    assert algo.spec_class == getattr(mod, algo.spec_class.__name__), \
+        "The spec_class must exist in the benchmark's module"
+    assert algo.data_class == getattr(mod, algo.data_class.__name__), \
+        "The data_class must exist in the benchmark's module"
+    assert algo.executable_generator_func == getattr(mod, algo.executable_generator_func.__name__), \
+        "the executable_generator_func must exist in the benchmark's module"
 
 
 def test_globally_unique_executable_family():
@@ -62,7 +68,8 @@ def test_gen_script(algo_config):
     algo, config = algo_config
 
     # Make sure it's formulaic
-    assert config.gen_script == f'gen-{config.short_name}.py'
+    assert config.gen_script == f'gen-{config.short_name}.py', \
+        "The gen_script should be of the form 'gen-{short_name}'"
 
     # Make sure it exists
     gen_script_path = (f"{RECIRQ_DIR}/{algo.domain.replace('.', '/')}/"
