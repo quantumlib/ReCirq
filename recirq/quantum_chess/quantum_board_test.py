68aad629a7c5dda12788af724c4dca2f07451b06
madcpf <madcpf@gmail.com>
2022-01-11 11:07:01 -0800

    remove test comments

--- recirq/quantum_chess/quantum_board_test.py
+++ recirq/quantum_chess/quantum_board_test.py
@@ -509,9 +509,6 @@ def test_slide_with_two_path_qubits_coherence():
     )
 
 
-# @pytest.mark.parametrize("board", BIG_CIRQ_BOARDS)
-# def test_split_slide_merge_slide_coherence(board):
-#    b = board(u.squares_to_bitboard(["b4", "d3"]))
 def test_split_slide_merge_slide_coherence():
     b = simulator(u.squares_to_bitboard(["b4", "d3"]))
     assert b.perform_moves(
