9fd10236d22e2c51949a34af2d982569286f1c30
Matthew Harrigan <mpharrigan@google.com>
2022-01-28 11:33:12 -0800

    improve scripts
    Change-Id: I25c45fbfdb4889d2a3a991e96cbfc8225ab6a206

--- recirq/otoc/loschmidt/tilted_square_lattice/run-simulator.py
+++ recirq/otoc/loschmidt/tilted_square_lattice/run-simulator.py
@@ -1,3 +1,23 @@
+# Copyright 2022 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A script for executing the loschmidt.tilted_square_lattice benchmark on a simulator.
+
+Practitioners should copy this script to provide their own parameters for
+QuantumRuntimeConfiguration.
+"""
+
 import cirq
 from cirq_google.workflow import QuantumRuntimeConfiguration, \
     SimulatedProcessorWithLocalDeviceRecord, execute
@@ -6,11 +26,11 @@ from recirq.otoc.loschmidt.tilted_square_lattice import TiltedSquareLatticeLosch
 
 assert TiltedSquareLatticeLoschmidtSpec, 'register deserializer'
 
-FN = 'loschmidt.tilted_square_lattice.small-v1.json.gz'
+EXES_FILENAME = 'loschmidt.tilted_square_lattice.small-v1.json.gz'
 
 
 def main():
-    exegroup = cirq.read_json_gzip(FN)
+    exegroup = cirq.read_json_gzip(EXES_FILENAME)
     rt_config = QuantumRuntimeConfiguration(
         processor_record=SimulatedProcessorWithLocalDeviceRecord('rainbow', noise_strength=0.005),
         run_id=get_unique_run_id('simulated-{i}'),
