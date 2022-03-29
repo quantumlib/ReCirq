24ab2de6e6d09c16d1177a98ad58b13e7d6834d3
Doug Strain <dstrain@google.com>
2022-03-29 13:13:59 -0700

    Upgrade CI requirements as well.

--- dev_tools/write-ci-requirements.py
+++ dev_tools/write-ci-requirements.py
@@ -8,9 +8,9 @@ REPO_DIR = pathlib.Path(__file__).parent.parent.resolve()
 print('Repo dir:', REPO_DIR)
 
 CIRQ_VERSIONS = {
-    'previous': '==0.12.0',
-    'current': '==0.13.0',
-    'next': '>=0.14.0.dev',
+    'previous': '==0.13.0',
+    'current': '==0.14.0',
+    'next': '>=0.15.0.dev',
 }
 """Give names to relative Cirq versions so CI can have consistent names while versions 
 get incremented."""
