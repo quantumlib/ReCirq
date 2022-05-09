ab01455a3c1fbb7e9b740874db6269e5e6db1abd
Matthew Harrigan <mpharrigan@google.com>
2022-05-09 11:15:23 -0700

    Use cirq.QubitPermutationGate in QAOA
    Change-Id: I00efa99bd49f8da4ffa151085061fc9c23cb52eb

--- recirq/qaoa/circuit_structure_test.py
+++ recirq/qaoa/circuit_structure_test.py
@@ -18,12 +18,6 @@ import pytest
 
 import cirq
 import cirq_google as cg
-
-try:
-    from cirq.interop.quirk import QuirkQubitPermutationGate
-except ImportError:
-    from cirq.contrib.quirk import QuirkQubitPermutationGate
-
 from recirq.qaoa.circuit_structure import get_moment_class, get_moment_classes, \
     find_circuit_structure_violations, BadlyStructuredCircuitError, validate_well_structured
 from recirq.qaoa.gates_and_compilation import ProblemUnitary, DriverUnitary, \
@@ -58,7 +52,7 @@ def test_get_moment_class():
 
     # Permutation
     assert get_moment_class(cirq.Moment([
-        QuirkQubitPermutationGate('', '', [1, 0]).on(q0, q1)])) == QuirkQubitPermutationGate
+        cirq.QubitPermutationGate([1, 0]).on(q0, q1)])) == cirq.QubitPermutationGate
 
 
 def test_get_moment_classes():
