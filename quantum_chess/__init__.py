1e991b09b30cd6551d7adcd2a5d094ef49cb316d
Ricardo Olenewa <rolenewa@gmail.com>
2022-06-22 10:04:36 -0400

    Make init friendly, restore workflows.

--- quantum_chess/__init__.py
+++ quantum_chess/__init__.py
@@ -1,3 +1,3 @@
 # Quantum Chess no longer exists in this repo. See README.md for mre info.
 
-raise ImportError
+raise ImportError("quantum_chess moved to https://github.com/quantumlib/unitary/tree/main/unitary/quantum_chess")
