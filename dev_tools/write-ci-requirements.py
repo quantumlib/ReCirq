60d145f0faedd66f7dd7054d4356d9c4b6aee318
Fionn Malone <fmalone@google.com>
2024-05-04 05:53:13 +0000

    ci requirements.

--- dev_tools/write-ci-requirements.py
+++ dev_tools/write-ci-requirements.py
@@ -75,7 +75,8 @@ def main(*, out_fn: str = 'ci-requirements.txt', relative_cirq_version: str = 'c
     core_reqs = _set_cirq_version(core_reqs, relative_cirq_version)
 
     extras_require = [
-        'otoc', 'qaoa', 'optimize', 'hfvqe', 'fermi_hubbard', 'qml_lfe', 'seniority_zero',
+        'otoc', 'qaoa', 'optimize', 'hfvqe', 'fermi_hubbard', 'qml_lfe',
+        'seniority_zero', 'qcqmc'
     ]
     extras_require = {
         r: _parse_requirements(pathlib.Path(REPO_DIR / f'recirq/{r}/extra-requirements.txt'))
