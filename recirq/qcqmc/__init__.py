422e07f9765d124f0aefd6b4034150534f01847a
Fionn Malone <fionn.malone@gmail.com>
2024-06-11 17:18:19 -0700

    Add __init__ + license.

--- recirq/qcqmc/__init__.py
+++ recirq/qcqmc/__init__.py
@@ -11,21 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from pathlib import Path
-
-DEFAULT_BASE_DATA_DIR = (Path(__file__).parent / "data").resolve()
-
-
-def get_integrals_path(
-    name: str, *, base_data_dir: str = DEFAULT_BASE_DATA_DIR
-) -> Path:
-    """Find integral data file by name.
-
-    Args:
-        name: The molecule name.
-    """
-    return Path(base_data_dir) / "integrals" / name / "hamiltonian.chk"
-
-
-_MOLECULE_INFO = {"fh_sto3g"}
--- recirq/qcqmc/data_test.py
+++ recirq/qcqmc/data_test.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import h5py
 import pytest
 
@@ -9,6 +23,7 @@ def test_default_base_data_dir():
     assert DEFAULT_BASE_DATA_DIR.exists()
     assert DEFAULT_BASE_DATA_DIR.is_dir()
     assert DEFAULT_BASE_DATA_DIR.name == "data"
+
 
 @pytest.mark.parametrize("name", ["fh_sto3g", "n2_ccpvtz"])
 def test_get_integrals_path(name):
