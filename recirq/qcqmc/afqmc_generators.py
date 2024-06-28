caa00461c3b9c1229f2dccc0135c91ffbcbad323
Fionn Malone <fmalone@google.com>
2024-06-28 21:37:59 +0000

    Add missing changes.

--- recirq/qcqmc/afqmc_generators.py
+++ recirq/qcqmc/afqmc_generators.py
@@ -46,11 +46,13 @@ def get_pp_plus_gate_generators(
         The list of generators necessary to construct the ansatz.
     """
     heuristic_gate_generators = get_heuristic_gate_generators(n_elec, heuristic_layers)
+    print(heuristic_gate_generators)
     if not do_pp:
         return heuristic_gate_generators
 
     n_pairs = n_elec // 2
     pair_gate_generators = get_pair_hopping_gate_generators(n_pairs, n_elec)
+    print(pair_gate_generators)
     return pair_gate_generators + heuristic_gate_generators
 
 
@@ -86,7 +88,7 @@ def get_charge_charge_generator(indices: Tuple[int, int]) -> of.FermionOperator:
     """Returns the generator for density evolution between the indices
 
     Args:
-        indices: The indices to for charge-charge terms.:w
+        indices: The indices to for charge-charge terms.
 
     Returns:
         The generator for density evolution for this pair of electrons.
