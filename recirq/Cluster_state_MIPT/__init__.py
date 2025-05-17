ddabe49fe29b5e7031949aa3d7b4ee7ebe40b14b
Wanda Hou <hwanda@ucsd.edu>
2025-05-17 13:19:15 -0700

    Delete recirq/Cluster_state_MIPT directory

--- recirq/Cluster_state_MIPT/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Cluster state measurement implementation at MIPT.
-
-This package contains implementations for creating and measuring cluster states
-on quantum processors.
-"""
-
-from recirq.Cluster_state_MIPT.cluster_state import get_two_qubits_6x6, get_circuit
-from recirq.Cluster_state_MIPT.data_collect import collect_data 
