34f1d72fddba3345e7f2740e5be52d9fff31bc63
Wanda Hou <hwanda@ucsd.edu>
2025-05-16 18:40:36 -0700

    Add files via upload

---
+++ recirq/Cluster_state_MIPT/__init__.py
@@ -0,0 +1,14 @@
+"""Cluster state circuit construction.
+
+This module provides tools for constructing cluster state circuits.
+"""
+
+from recirq.Cluster_state_MIPT.cluster_state import (
+    get_circuit,
+    get_two_qubits_6x6
+)
+
+__all__ = [
+    'get_circuit',
+    'get_two_qubits_6x6'
+] 
---
+++ recirq/Cluster_state_MIPT/setup.py
@@ -0,0 +1,14 @@
+"""Setup file for the Cluster_state_MIPT module."""
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="recirq-cluster-state",
+    version="0.1.0",
+    packages=find_namespace_packages(include=["recirq.*"]),
+    install_requires=[
+        "cirq>=0.13.0",
+        "numpy>=1.19.0",
+    ],
+    python_requires=">=3.7",
+) 
