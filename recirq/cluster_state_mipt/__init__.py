46652bf0b6446674a8f3e26dc40eb3379c504fe5
Wanda Hou <hwanda@ucsd.edu>
2025-05-19 13:07:14 -0700

    Update __init__.py

--- recirq/cluster_state_mipt/__init__.py
+++ recirq/cluster_state_mipt/__init__.py
@@ -1,8 +1,16 @@
-"""Cluster state measurement implementation at MIPT.
-
-This package contains implementations for creating and measuring cluster states
-on quantum processors.
-"""
+# Copyright 2025 Google
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from recirq.cluster_state_mipt.cluster_state import get_two_qubits_6x6, get_circuit
 from recirq.cluster_state_mipt.data_collect import collect_data 
