5b603d0938e7c5bdc353c964c9f9918d80fe3499
Alejo Gradau <alejogradau@google.com>
2026-02-20 23:03:56 +0000

    solving errors?

--- recirq/contextuality/magic_square_game_test.py
+++ recirq/contextuality/magic_square_game_test.py
@@ -7,7 +7,7 @@ import recirq.contextuality.magic_square_game as msg
 
 @pytest.mark.parametrize(
     "game",
-    ["guess_3rd", "measure_3rd_classical_multiplication", "measure_3rd_quantum_multiplication"],
+    ["infer_3rd", "measure_3rd_classical_multiplication", "measure_3rd_quantum_multiplication"],
 )
 @pytest.mark.parametrize("add_dd", [True, False])
 def test_run_contextuality_experiment(game: msg.GameType, add_dd: bool) -> None:
@@ -30,20 +30,20 @@ def test_run_contextuality_experiment(game: msg.GameType, add_dd: bool) -> None:
 
 @pytest.mark.parametrize(
     "game",
-    ["guess_3rd", "measure_3rd_classical_multiplication", "measure_3rd_quantum_multiplication"],
+    ["infer_3rd", "measure_3rd_classical_multiplication", "measure_3rd_quantum_multiplication"],
 )
 @pytest.mark.parametrize("add_dd", [True, False])
 def test_invalid_input_raises(game: msg.GameType, add_dd: bool):
     sampler = cirq.Simulator()
     alice_qubits = cirq.GridQubit.rect(1, 4, 0, 0)  # test with 2 measure qubits
     bob_qubits = cirq.GridQubit.rect(1, 4, 1, 0)
-    with pytest.raises(ValueError, match="guess_3rd if you sub_case = only_two_qubits"):
+    with pytest.raises(ValueError, match="infer_3rd if you sub_case = only_two_qubits"):
         _ = msg.run_contextuality_experiment(
             sampler,
             alice_qubits,
             bob_qubits,
             game=game,
             add_dd=add_dd,
-            sub_case="square_1",
+            sub_case="only_two_qubits",
             repetitions=10,
         )
