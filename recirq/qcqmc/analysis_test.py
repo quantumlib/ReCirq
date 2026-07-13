cdf76fba7a7847d170cf5113e8e93fc1d5017afb
mhucka <mhucka@google.com>
2026-07-13 01:27:03 +0000

    Move "seed" changes to separate PR

--- recirq/qcqmc/analysis_test.py
+++ recirq/qcqmc/analysis_test.py
@@ -90,7 +90,7 @@ def test_small_experiment_partition_match_reversed_partition_same_seed(
         n_samples_per_clifford=n_samples_per_clifford,
         noise_model_name="None",
         noise_model_params=(0,),
-        seed=1,
+        seed=2,
     )
 
     experiment_1 = ExperimentData.build_experiment_from_dependencies(
--- recirq/qcqmc/experiment.py
+++ recirq/qcqmc/experiment.py
@@ -143,7 +143,7 @@ def get_samples_from_simulation(
             circuits.
 
     """
-    simulator = qsimcirq.QSimSimulator(seed=seed)
+    simulator = qsimcirq.QSimSimulator()
 
     sampled_bitstrings = []
 
